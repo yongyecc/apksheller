ba9b22927eedd122060ce51f251d3ebd5d477c71
yong夜 <1014885794@qq.com>
2021-09-16 22:37:14 +0800

    Update StubApp.java

--- shellApplicationSourceCode/java/cn/yongye/stub/StubApp.java
+++ shellApplicationSourceCode/java/cn/yongye/stub/StubApp.java
@@ -7,7 +7,9 @@ import android.content.pm.ApplicationInfo;
 import android.content.res.AssetManager;
 import android.os.Environment;
 import android.util.Log;
-
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import dalvik.system.BaseDexClassLoader;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.File;
@@ -51,23 +53,7 @@ public class StubApp extends Application {
             e.printStackTrace();
         }
         Log.i(TAG, "解密原始DEXcompleted");
-//        InputStream oIn = null;
-//        FileOutputStream fos = null;
-//        try {
-//            fos = new FileOutputStream(new File(stDmpDexName));
-//            byte[] ba = new byte[1024];
-//            int len;
-//            while((len = oIn.read(ba)) != -1){
-//                for (int i=0; i<len; i++)
-//                    ba[i] ^= bKey;
-//                fos.write(ba, 0, len);
-//
-//            }
-//        } catch (IOException e) {
-//            Log.e(TAG, "not found " + stSrcDexName);
-//            e.printStackTrace();
-//        }
-//        Log.i(TAG, "decrypt completed.");
+        
 
         /**
          * 2.加载原始DEX
@@ -86,6 +72,8 @@ public class StubApp extends Application {
         DexClassLoader oDexClassLoader = new DexClassLoader(stDmpDexPt, oApp.getCacheDir().getAbsolutePath(),
                 nativeLib.getAbsolutePath(), (ClassLoader)RefInvoke.getFieldObject(stClassLoadedApk,
                 wr.get(), "mClassLoader"));
+
+        // load(oApp, oDexClassLoader, "/data/local/tmp/classes2.dex");
         RefInvoke.setFieldObject(stClassLoadedApk, "mClassLoader", wr.get(), oDexClassLoader);
         Log.i(TAG, "load source dex completed.");
 
@@ -114,5 +102,79 @@ public class StubApp extends Application {
         }
         makeApplication.onCreate();
         Log.i(TAG, "unshell completed.");
+    }
+
+    public void load(Context oApp, DexClassLoader dcl, String path) {
+        try {
+            // 已加载的dex
+            Object dexPathList = getField(BaseDexClassLoader.class, "pathList", oApp.getClassLoader());
+            Object dexElements = getField(dexPathList.getClass(), "dexElements", dexPathList);
+
+            // patchdex
+            String dexOptDir = oApp.getCacheDir().getAbsolutePath();
+            Object patchDexPathList = getField(BaseDexClassLoader.class, "pathList", dcl);
+            Object patchDexElements = getField(patchDexPathList.getClass(), "dexElements", patchDexPathList);
+
+            // 将patchdex和已加载的dexes数组拼接连接
+            Object concatDexElements = concatArray(patchDexElements, dexElements);
+
+            // 重新给dexPathList#dexElements赋值
+            setField(dexPathList.getClass(), "dexElements", dexPathList, concatDexElements);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * @param cls       被访问对象的class
+     * @param fieldName 对象的成员变量名
+     * @param object    被访问对象
+     * @return
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public Object getField(Class<?> cls, String fieldName, Object object) throws NoSuchFieldException, IllegalAccessException {
+        Field field = cls.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(object);
+    }
+
+
+    /**
+     * @param cls       被访问对象的class
+     * @param fieldName 对象的成员变量名
+     * @param object    被访问对象
+     * @param value     赋值给成员变量
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    public void setField(Class<?> cls, String fieldName, Object object, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field field = cls.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(object, value);
+    }
+
+
+    /**
+     * 连接两个数组（指定位置）
+     *
+     * @param left  连接后在新数组的左侧
+     * @param right 连接后在新数组的右侧
+     * @return
+     */
+    public Object concatArray(Object left, Object right) {
+        int len1 = Array.getLength(left);
+        int len2 = Array.getLength(right);
+        int totalLen = len1 + len2;
+        Object concatArray = Array.newInstance(left.getClass().getComponentType(), totalLen);
+        for(int i = 0; i < len1; i++) {
+            Array.set(concatArray, i, Array.get(left, i));
+        }
+        for(int j = 0; j < len2; j++) {
+            Array.set(concatArray, len1 + j, Array.get(right, j));
+        }
+        return concatArray;
     }
 }
